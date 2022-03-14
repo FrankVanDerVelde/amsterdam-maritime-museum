53882469d3e6d2fd13cd8eaf61e40562457c6eeb
Frank <frankvdv1998@gmail.com>
2022-03-15 00:41:06 +0100

    Added some more unique trees and added to tree background functionality

--- src/assets/js/treeCanvas.js
+++ src/assets/js/treeCanvas.js
@@ -1,12 +1,21 @@
 const app = new PIXI.Application({
-    autoResize: true,
     transparent: true,
-    resolution: devicePixelRatio
+    autoResize: true,
+    width: window.innerHeight,
+    height: window.innerWidth,
+    resolution: devicePixelRatio,
+    autoDensity: true
 });
 
 const canvasDiv = document.getElementById("canvas-box");
 canvasDiv.appendChild(app.view);
 
+canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+// amount of unique trees in the assets folder
+const uniqueTrees = 5;
 
 // Resize ability for canvas
 window.addEventListener('resize', resize);
@@ -28,9 +37,22 @@ function getRandomY() {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-const treeCount = 20;
+let treeCount = 35;
+// setInterval(function () {
+//     let sprite = PIXI.Sprite.from(`../assets/images/trees/tree${Math.floor(Math.random() * (6))}.png`);
+
+//     sprite.x = getRandomX();
+//     sprite.y = getRandomY();
+
+//     sprite.width = 70;
+//     sprite.height = 70;
+
+//     app.stage.addChild(sprite);
+// }, 1000);
+
 for (i = 0; i < treeCount; i++) {
-    let sprite = PIXI.Sprite.from('../assets/images/tree-example.png');
+    
+    let sprite = PIXI.Sprite.from(`../assets/images/trees/tree${Math.floor(Math.random() * (6))}.png`);
 
     sprite.x = getRandomX();
     sprite.y = getRandomY();
