fe8453358b5a4792b7ef54dd87db03bc44446b8e
D.T.Verveer <dennis.verveer@hva.nl>
2022-05-17 00:02:49 +0200

    license plate start point html + css + js

--- src/assets/js/controllers/chooseVehicleController.js
+++ src/assets/js/controllers/chooseVehicleController.js
@@ -1,16 +1,17 @@
 /**
  * Controller for the calculator
  */
-import { App } from "../app.js";
-import { Controller} from "./controller.js";
+import {App} from "../app.js";
+import {Controller} from "./controller.js";
 
 export class ChooseVehicleController extends Controller {
     #chooseVehicleView;
     #chosenVehicle;
+    #vehicle
 
     constructor() {
         super();
-        this.#setupView().then( () => {
+        this.#setupView().then(() => {
             this.#addContinueButtonEventListener();
         });
     }
@@ -19,32 +20,28 @@ export class ChooseVehicleController extends Controller {
         this.#chooseVehicleView = await super.loadHtmlIntoContent("html_views/chooseVehicle.html");
 
         const test = this.#chooseVehicleView;
-        console.log(this.#chooseVehicleView);
+        this.#savingChosenVehicle();
+        this.#showsContinueButton(false);
+        console.log(this.#vehicle);
 
         const buttonCards = test.querySelectorAll('.btn_card');
+
+        this.#chooseVehicleView.querySelector(".btn_card").addEventListener('click', this.#savingChosenVehicle());
 
         buttonCards.forEach(card => {
             card.addEventListener('click', function (e) {
                 const active = test.querySelector('.btn_card.active');
 
-
-                console.log(active);
-
-
-
                 if (active) {
                     active.classList.toggle('active');
-
-                    // test.querySelector('.btn_card .active').classList.toggle('active');
                 }
 
                 card.classList.toggle('active');
 
                 const car = test.querySelector('.car');
-                if (car.classList.contains('active')){
+                if (car.classList.contains('active')) {
                     test.querySelector('#licensePlate').hidden = false;
-                    console.log("Wooo");
-                }else {
+                } else {
                     test.querySelector('#licensePlate').hidden = true;
                 }
             })
@@ -56,13 +53,51 @@ export class ChooseVehicleController extends Controller {
         continueContainer.onclick = this.#handleContinueButtonClicked;
     }
 
-    #savingChosenVehicle(){
-        
+    #savingChosenVehicle() {
+        this.#chosenVehicle = this.#chooseVehicleView.querySelector(this.#getElementByIdId())
+        switch (this.#chosenVehicle) {
+            case "walk" :
+                this.#vehicle = "walk"
+                break;
+            case "bike" :
+                this.#vehicle = "bike"
+                break;
+            case "car" :
+                this.#vehicle = "car"
+                break;
+            case "bus" :
+                this.#vehicle = "bus"
+                break;
+            case "train" :
+                this.#vehicle = "train"
+                break;
+            case "tram" :
+                this.#vehicle = "tram"
+        }
+        console.log(this.#vehicle)
+        return this.#vehicle;
+    }
+
+    #canContinue() {
+        return (this.#vehicle !== undefined)
     }
 
     #handleContinueButtonClicked() {
-        // App.loadController(App.CONTROLLER_TREE_BACKGROUND);
-        App.loadController(App.CONTROLLER_CHOOSE_FUEL);
+        if (this.#canContinue()){
+            window.localStorage.setItem('chosenVehicle', this.#vehicle)
+            App.loadController(App.CONTROLLER_CHOOSE_FUEL);
+        }
+    }
 
+    #showsContinueButton(canContinue) {
+        this.#getContinueContainer().style.opacity = canContinue ? "1" : "0.6";
+    }
+
+    #getContinueContainer() {
+        return this.#getElementByIdId('continue-container');
+    }
+
+    #getElementByIdId(id) {
+        return this.#chooseVehicleView.querySelector(`#${id}`);
     }
 }
