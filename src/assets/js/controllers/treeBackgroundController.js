83d6f723e8aaa9a00aec6d27b0c92ca79490784d
Frank <frankvdv1998@gmail.com>
2022-05-18 23:12:55 +0200

    Moved sprite creation functions into a seperate module

--- src/assets/js/controllers/treeBackgroundController.js
+++ src/assets/js/controllers/treeBackgroundController.js
@@ -7,6 +7,7 @@ import decorative_sprites from "../../json/decorative-sprites.js"
 import { calculatorRepository } from "../repositories/calculatorRepository.js";
 import {NetworkManager} from "../framework/utils/networkManager.js";
 import { NSRepository } from "../repositories/NSRepository.js";
+import { createBasicSprite, createSideScrollingSprites } from "../sprite-functions/sprite-creation.js";
 
 export class TreeBackgroundController extends Controller {
     #calculatorRepository;
@@ -260,8 +261,8 @@ export class TreeBackgroundController extends Controller {
             return cloud;
         })
 
-        const boatSpriteReferences = this.#createSideScrollingSprites(boatSprites, boatSheet)
-        const cloudSpriteReferences = this.#createSideScrollingSprites(cloudSprites, cloudSheet)
+        const boatSpriteReferences = createSideScrollingSprites(boatSprites, boatSheet, app, canvasDiv.offsetWidth);
+        const cloudSpriteReferences = createSideScrollingSprites(cloudSprites, cloudSheet, app, canvasDiv.offsetWidth);
 
         // update y pos
         window.addEventListener('resize', () => {
@@ -312,92 +313,6 @@ export class TreeBackgroundController extends Controller {
         resize();
     }
 
-    #createBasicSprite(spriteObject, sheet) {
-        const sprite = PIXI.Sprite.from(sheet.textures[spriteObject.img]);
-
-        if (!spriteObject.height || spriteObject.height === 'auto') {
-            const sizePercentageOfOriginalImage = (spriteObject.width * 100) / sprite.width;
-            sprite.height = Math.floor((sprite.height * sizePercentageOfOriginalImage) / 100);
-        } else {
-            sprite.height = spriteObject.height;
-        }
-
-        sprite.x = spriteObject.basePosX;
-        sprite.y = spriteObject.basePosY;
-
-        sprite.width = spriteObject.width;
-
-        // If the sprite is going in the opposite direction flip it
-        if (spriteObject.direction && spriteObject.direction === 'left') {
-            sprite.scale.x = -sprite.scale.x;
-        }
-
-        spriteObject.zIndex && (sprite.zIndex = spriteObject.zIndex);
-
-        // Sets the sprites anchor to bottom, center
-        sprite.anchor.set(0.5, 1);
-
-        return sprite;
-    }
-
-    #createSideScrollingSprites(spritesArray, sheet) {
-        const createBasicSprite = this.#createBasicSprite;
-        const app = this.#canvasApp;
-        const canvasDiv = this.#treeBackgroundView.querySelector("#canvas-box");
-
-        const spritesObjectArray = [];
-
-        spritesArray.forEach(spriteObject => {
-            const originalSprite = createBasicSprite(spriteObject, sheet);
-            const spriteCopy = createBasicSprite(spriteObject, sheet);
-
-            spritesObjectArray.push({ "sprite": originalSprite, "sprite_copy": spriteCopy })
-
-            spriteCopy.visible = false;
-
-            app.stage.addChild(originalSprite);
-            app.stage.addChild(spriteCopy);
-
-            let spriteOneActive = true;
-            let spriteTwoActive = false;
-
-            let speed = spriteObject.speed ? spriteObject.speed : 1;
-
-            app.ticker.add((delta) => {
-                // Note: The sprites x pos is when checking is in the middle of the image
-                const leavingScreenPos = spriteObject.direction === 'right' ? canvasDiv.offsetWidth - originalSprite.width / 2 : originalSprite.width / 2;
-                const fullyLeftScreenPos = spriteObject.direction === 'right' ? canvasDiv.offsetWidth + originalSprite.width / 2 : -(originalSprite.width / 2);
-                const offScreenStartPos = spriteObject.direction === 'right' ? -spriteObject.width : canvasDiv.offsetWidth + spriteObject.width;
-                let movementChange = spriteObject.direction === 'right' ? speed : -(speed);
-                movementChange = parseFloat(movementChange.toFixed(2));
-
-                // console.log(canvasDiv.offsetWidth)
-
-                [originalSprite, spriteCopy].forEach(sprite => {
-                    if (Math.floor(sprite.x) === leavingScreenPos) {
-                        spriteTwoActive = true;
-                    }
-
-                    // When the sprite touches the edge of the screen with it's back
-                    if (Math.floor(sprite.x) === fullyLeftScreenPos) {
-                        spriteOneActive = false;
-                        originalSprite.x = offScreenStartPos;
-                    }
-                });
-
-                [spriteOneActive, spriteTwoActive].forEach(spriteState => {
-                    if (spriteState === true) {
-                        originalSprite.x += movementChange;
-                    }
-                });
-
-            });
-
-        });
-
-        return spritesObjectArray;
-    }
-
     async #manageTrees() {
         const canvas = this.#canvasApp;
         const treeSheet = this.#treeSheet;
@@ -412,8 +327,6 @@ export class TreeBackgroundController extends Controller {
         // The array that hold all the tree sprites
 
         const treeContainer = this.#pixiTreeContainer;
-
-        const createBasicSprite = this.#createBasicSprite;
 
         function getRandomX() {
             const min = Math.floor(0);
